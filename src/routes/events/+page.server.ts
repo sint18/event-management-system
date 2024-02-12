d9950606ff08d250cb2ca3af458198cd154dc69e
sint18 <htoohtoo.slh@gmail.com>
2024-02-12 12:26:08 +0000

    display events implemented

---
+++ src/routes/events/+page.server.ts
@@ -0,0 +1,15 @@
+import * as db from '$lib/server/db'
+export async function load() {
+	const result = await db.query(`
+	select event_id as "Event ID",
+	       event_name as "Event Name",
+	       description as "Description",
+	       to_char(start_date, 'DD/MM/YYYY') as "Start Date",
+         to_char(end_date, 'DD/MM/YYYY') as "End Date",
+	       venue as "Venue",
+	       upper(status) as "Status"
+	    from events;
+	`, [])
+	const headers = Object.keys(result.rows[0]);
+	return { allEvents: result.rows, headers: headers}
+}
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,5 +1,36 @@
+<script lang="ts">
+	import { Table } from '@skeletonlabs/skeleton';
+	import type { TableSource } from '@skeletonlabs/skeleton';
+	import { tableMapperValues } from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
+
+	export let data
+
+	function selectionHandler(meta: CustomEvent) {
+		if (meta.detail) {
+			goto(`events/${meta.detail[0]}`);
+		}
+	}
+
+	function setTableSource(): TableSource {
+		return {
+			head: data.headers,
+			body: tableMapperValues(data.allEvents, data.headers),
+			meta: tableMapperValues(data.allEvents, data.headers),
+		};
+	}
+
+	// If sourceData updates, set the new TableSource values
+	$: tableSimple = data.allEvents ? setTableSource() : undefined;
+
+
+</script>
 <div class="p-10 space-y-5 container h-full justify-center">
 	<h2 class="h2">Events</h2>
 	<hr />
-	<p>Display Events</p>
+	<a href="events/create" class="btn variant-filled">Create Event</a>
+	{#if tableSimple}
+		<p>Display Events</p>
+		<Table source={tableSimple} interactive={true} on:selected={selectionHandler} />
+	{/if}
 </div>
