9832d2ada711c930db5c6213443bd3e9cc31500e
sint18 <htoohtoo.slh@gmail.com>
2024-02-25 17:41:26 +0000

    implemented updating users

--- src/routes/users/[userId]/+page.server.ts
+++ src/routes/users/[userId]/+page.server.ts
@@ -1,5 +1,5 @@
 import * as db from '$lib/server/db'
-import { error } from '@sveltejs/kit'
+import { error, fail } from '@sveltejs/kit'
 
 export async function load({ params }){
 	const userId = params.userId
@@ -8,7 +8,7 @@ export async function load({ params }){
 		error(404, 'Not Found')
 	}
 
-	const queryResult = await db.query(`
+	const queryUserInfo = await db.query(`
 	select users.user_id,
        users.username,
        users.first_name,
@@ -16,6 +16,7 @@ export async function load({ params }){
        users.email,
        users.points,
        r.role_name,
+       users.role_id,
 			 to_char(users.last_updated, 'YYYY-MM-DD HH24:MI:SS') as last_updated,
 			 to_char(users.created_at, 'YYYY-MM-DD HH24:MI:SS') as created_at
 	from users
@@ -23,7 +24,52 @@ export async function load({ params }){
 	where users.user_id = $1;
 	`, [userId])
 
-	if(queryResult.rowCount && queryResult.rowCount > 0) {
-		return { userInfo: queryResult.rows[0] }
+	const queryRoles = await db.query(`
+	select role_id, role_name from roles;
+	`, [])
+
+	if(queryUserInfo.rowCount && queryRoles.rowCount && queryUserInfo.rowCount > 0 && queryRoles.rowCount > 0) {
+		return { userInfo: queryUserInfo.rows[0], roles: queryRoles.rows }
+	}
+}
+
+export const actions = {
+	updateUser: async({ request }) => {
+		const data = await request.formData();
+		// console.log(data);
+		const userId: string = <string>data.get('userId')
+		const roleId: string = <string>data.get('roleId')
+		const firstName: string = <string>data.get('firstName')
+		const lastName: string = <string>data.get('lastName')
+		const username: string = <string>data.get('username')
+		const email: string = <string>data.get('email')
+
+		if (!firstName) {
+			return fail(400, {error: true, errMessage: 'First Name field cannot be empty'})
+		}
+		if (!lastName) {
+			return fail(400, {error: true, errMessage: 'Last Name field cannot be empty'})
+		}
+		if (!email) {
+			return fail(400, {error: true, errMessage: 'Email field cannot be empty'})
+		}
+		if (!username) {
+			return fail(400, {error: true, errMessage: 'Username field cannot be empty'})
+		}
+
+		const queryUpdate = await db.query(`
+		update users
+		set role_id      = $1,
+				first_name   = $2,
+				last_name    = $3,
+				username     = $4,
+				email        = $5,
+				last_updated = now()
+		where user_id = $6;
+		`, [roleId, firstName, lastName, username, email, userId])
+
+		if (queryUpdate.rowCount && queryUpdate.rowCount > 0) {
+			return { success: true }
+		}
 	}
 }
--- src/routes/users/[userId]/+page.svelte
+++ src/routes/users/[userId]/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-	import type { PopupSettings } from '@skeletonlabs/skeleton';
-	import { popup } from '@skeletonlabs/skeleton';
+	import type { ModalSettings, PopupSettings } from '@skeletonlabs/skeleton';
+	import { popup, getModalStore } from '@skeletonlabs/skeleton';
 	import { invalidateAll } from '$app/navigation';
+	import { enhance } from '$app/forms';
 
 	export let data
+	export let form
 	let readOnly: boolean = true
+	let formElement: HTMLFormElement
+	const modelStore = getModalStore()
+
+	// ---------------- Functions ----------------------------
+
+ function submitUpdateReq(response: boolean){
+		if(response) {
+			formElement.action = "?/updateUser"
+			formElement.requestSubmit()
+		}
+ }
 
 	// ---------------- Dropdown Config ----------------------
 	let comboboxValue: string = 'actions'
@@ -15,11 +28,23 @@
 		placement: 'right'
 	};
 
+	// --------------- Modal Config -------------------------
+	const updateModal: ModalSettings = {
+		type: 'confirm',
+		// Data
+		title: 'Please Confirm Update',
+		body: 'Are you sure you wish to proceed?',
+		buttonTextConfirm: 'Update',
+		// TRUE if confirm pressed, FALSE if cancel pressed
+		response: (r: boolean) => submitUpdateReq(r),
+	};
+
 </script>
 <!-- #TODO: view, edit and delete bookings of each user -->
 <div class="p-10 space-y-5 container h-full justify-center">
 	<button type="button" on:click={() => {history.back()}} class="btn variant-filled-surface">Back</button>
 	<h2 class="h2">User Information</h2>
+	<hr>
 
 	{#if data.userInfo}
 
@@ -29,69 +54,89 @@
 
 		<div class="card p-4 w-48" data-popup="popupCombobox">
 			<div class="grid grid-cols-1 gap-2">
-				<button type="button" class="btn bg-transparent">Refresh</button>
+				<button type="button" class="btn bg-transparent" on:click={() => {invalidateAll(); readOnly = true}}>Refresh</button>
 				<button type="button" class="btn bg-transparent" on:click={() => {readOnly = false}}>Edit Information</button>
 				<button class="btn variant-ghost-error">Delete User</button>
 			</div>
 		</div>
-
-		<div class="grid grid-cols-2 gap-4">
+		{#if form?.success }
+			<p class="alert variant-ghost-success">Record Successfully Updated!</p>
+		{/if}
+		{#if form?.error }
+			<p class="alert variant-ghost-error">{form.errMessage}</p>
+		{/if}
+		<form class="grid grid-cols-2 gap-4" method="post" use:enhance bind:this={formElement}>
 
 			<label class="label">
 				<span>User ID</span>
-				<input class="input" type="number" placeholder="" value={data.userInfo['user_id']} readonly={true}/>
+				<input name="userId" class="input" type="number" placeholder="" bind:value={data.userInfo['user_id']} readonly={true}/>
 			</label>
 
 			<label class="label">
 				<span>Role</span>
-				<input class="input uppercase" type="text" placeholder="" value={data.userInfo['role_name']} readonly={true}/>
+				{#if readOnly}
+					<input class="input uppercase" type="text" placeholder="" bind:value={data.userInfo['role_name']} readonly={true}/>
+				{:else}
+					{#if data.roles }
+						<select name="roleId" class="select uppercase" bind:value={data.userInfo['role_id']} >
+							{#each data.roles as row}
+								<option class="uppercase" value={row['role_id']}>{row['role_name']}</option>
+							{/each}
+						</select>
+					{/if}
+				{/if}
 			</label>
+
 
 			<label class="label">
 				<span>First Name</span>
-				<input class="input" type="text" placeholder="" value={data.userInfo['first_name']} readonly={readOnly}/>
+				<input name="firstName" class="input" type="text" placeholder="Enter first name" bind:value={data.userInfo['first_name']} readonly={readOnly}/>
 			</label>
 
 			<label class="label">
 				<span>Last Name</span>
-				<input class="input" type="text" placeholder="" value={data.userInfo['last_name']} readonly={readOnly}/>
+				<input name="lastName" class="input" type="text" placeholder="Enter last name" bind:value={data.userInfo['last_name']} readonly={readOnly}/>
 			</label>
 
 			<label class="label">
 				<span>User Name</span>
-				<input class="input" type="text" placeholder="" value={data.userInfo['username']} readonly={readOnly}/>
+				<input name="username" class="input" type="text" placeholder="Enter username" bind:value={data.userInfo['username']} readonly={readOnly}/>
 			</label>
 
 			<label class="label">
 				<span>Email Address</span>
-				<input class="input" type="email" placeholder="" value={data.userInfo['email']} readonly={readOnly}/>
+				<input name="email" class="input" type="email" placeholder="Enter email address" bind:value={data.userInfo['email']} readonly={readOnly}/>
 			</label>
 
 			<div class="grid grid-cols-subgrid gap-4 col-span-2">
 				<div class="col-start-1">
 					<label class="label">
 						<span>Earned Points</span>
-						<input class="input" type="number" placeholder="" value={data.userInfo['points']} readonly={true}/>
+						<input class="input" type="number" placeholder="" bind:value={data.userInfo['points']} readonly={true}/>
 					</label>
 				</div>
 			</div>
 
 			<label class="label">
 				<span>Last Updated</span>
-				<input class="input variant-ghost" type="datetime-local" placeholder="" value={data.userInfo['last_updated']} readonly={true}/>
+				<input class="input variant-ghost" type="datetime-local" placeholder="" bind:value={data.userInfo['last_updated']} readonly={true}/>
 			</label>
 
 			<label class="label">
 				<span>Created Date</span>
-				<input class="input variant-ghost" type="datetime-local" placeholder="" value={data.userInfo['created_at']} readonly={true}/>
+				<input class="input variant-ghost" type="datetime-local" placeholder="" bind:value={data.userInfo['created_at']} readonly={true}/>
 			</label>
 
 			{#if !readOnly}
 				<div class="grid grid-cols-6 gap-4">
-					<button type="button" class="btn variant-filled-primary">Save</button>
+					<button type="button" class="btn variant-filled-primary" on:click={() => modelStore.trigger(updateModal)}>Save</button>
 					<button type="button" class="btn variant-filled" on:click={() => {invalidateAll(); readOnly = true}}>Cancel</button>
 				</div>
 			{/if}
-		</div>
+		</form>
+		<h3 class="h3">Bookings</h3>
+		<hr>
+
+
 	{/if}
 </div>
