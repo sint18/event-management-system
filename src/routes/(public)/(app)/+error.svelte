acf8ecec90572ef5c0427f7f2a5e65621c311e9c
sint18 <htoohtoo.slh@gmail.com>
2024-03-18 21:08:00 +0000

    error handling implemented

---
+++ src/routes/(public)/(app)/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="p-5 space-y-5 container mx-auto h-full justify-center">
+	{#if $page.error}
+		<p class="alert variant-ghost-error">{$page.error.message}</p>
+	{/if}
+</div>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.error}
+	<h1 class="h1">{$page.error.message}</h1>
+{/if}
