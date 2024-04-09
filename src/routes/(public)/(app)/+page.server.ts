dacc958e6e1f2d3552045445520df6b22ba0ab31
sint18 <htoohtoo.slh@gmail.com>
2024-04-09 22:39:13 +0100

    implemented filtering events by organizer, category and status

--- src/routes/(public)/(app)/+page.server.ts
+++ src/routes/(public)/(app)/+page.server.ts
@@ -1,6 +1,30 @@
-import * as db from '$lib/server/db'
-export async function load() {
-	const queryResult = await db.query(`
+import * as db from '$lib/server/db';
+
+export async function load({ url }) {
+
+	const categoryFilter = url.searchParams.get('category')
+	const organizerFilter = url.searchParams.get('organizer')
+	const statusFilter = url.searchParams.get('status')
+
+
+	let queryResult
+
+	if (categoryFilter && organizerFilter && statusFilter) {
+		queryResult = await db.query(`
+        select id                                               as event_id,
+               event_name,
+               description,
+               to_char(start_datetime, 'DD/MM/YYYY HH24:MI:SS') as start_datetime,
+               to_char(end_datetime, 'DD/MM/YYYY HH24:MI:SS')   as end_datetime,
+               (end_datetime - start_datetime)::text            as duration,
+               location,
+               status
+        from events
+        where category_id = $1 and organizer_id = $2 and status = $3
+        order by events.start_datetime desc;
+		`, [categoryFilter, organizerFilter, statusFilter]);
+	} else {
+		queryResult = await db.query(`
       select id                                               as event_id,
              event_name,
              description,
@@ -8,12 +32,29 @@ export async function load() {
              to_char(end_datetime, 'DD/MM/YYYY HH24:MI:SS')   as end_datetime,
              (end_datetime - start_datetime)::text            as duration,
              location,
-             upper(status)
+             status
       from events
       order by events.start_datetime desc;
-	`, [])
-	if (queryResult.rowCount && queryResult.rowCount > 0) {
-		return { events: queryResult.rows }
+		`, []);
 	}
-	return { events: [] }
+
+	const categoryQuery = await db.query(`
+      select id, category_name
+      from event_category
+      where active is true;
+	`, []);
+
+	const organizerQuery = await db.query(`
+      select id, organizer_name
+      from organizers
+      where active is true;
+	`, []);
+
+	return {
+		events: queryResult.rows,
+		filters: {
+			categories: categoryQuery.rows,
+			organizers: organizerQuery.rows
+		}
+	};
 }
--- src/routes/(public)/(app)/+page.svelte
+++ src/routes/(public)/(app)/+page.svelte
@@ -1,17 +1,102 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 	import CalendarClock from 'virtual:icons/mdi/calendar-clock';
-	import DurationIcon from 'virtual:icons/mdi/clockwise';
+	import StatusIcon from 'virtual:icons/mdi/tick-circle-outline';
 	import MapIcon from 'virtual:icons/mdi/map-marker-radius';
 	import ArrowIcon from 'virtual:icons/mdi/arrow-down-drop-circle-outline';
+	import FilterIcon from 'virtual:icons/mdi/filter-variant';
+	import FilterOffIcon from 'virtual:icons/mdi/filter-off-outline';
 	export let data
+
+	let categoryFilter: string
+	let organizerFilter: string
+	let statusFilter: string
+
+	function checkStatus(status: string) {
+		if (status === 'upcoming') {
+			// return 'text-success-500'
+			return 'variant-filled-success'
+		} else {
+			// return 'text-error-500'
+			return 'variant-filled-error'
+		}
+	}
+
+	function submitHandler() {
+		$page.url.searchParams.set('category', categoryFilter)
+		$page.url.searchParams.set('organizer', organizerFilter)
+		$page.url.searchParams.set('status', statusFilter)
+		return goto($page.url, {invalidateAll: true})
+	}
+
+	function resetFilter() {
+		$page.url.searchParams.delete('category')
+		$page.url.searchParams.delete('organizer')
+		$page.url.searchParams.delete('status')
+		return goto($page.url, {invalidateAll: true})
+	}
+
 </script>
 <div class="p-5 space-y-5 container mx-auto h-full justify-center">
 	<h1 class="h1">Home</h1>
+
+	<Accordion>
+		<AccordionItem>
+			<svelte:fragment slot="lead"><FilterIcon></FilterIcon></svelte:fragment>
+			<svelte:fragment slot="summary"><h4 class="h4">Filter Options</h4></svelte:fragment>
+			<svelte:fragment slot="content">
+
+				<form class="space-y-4" on:change|preventDefault={submitHandler}>
+					{#if data.filters.categories}
+					<label class="label">
+						<span>Category</span>
+						<select class="select" bind:value={categoryFilter}>
+							{#each data.filters.categories as row }
+							<option value={row['id']}>{row['category_name']}</option>
+							{/each}
+						</select>
+					</label>
+					{/if}
+
+					{#if data.filters.organizers}
+					<label class="label">
+						<span>Organizer</span>
+						<select class="select" bind:value={organizerFilter}>
+							{#each data.filters.organizers as row }
+							<option value={row['id']}>{row['organizer_name']}</option>
+							{/each}
+						</select>
+					</label>
+					{/if}
+
+					<label class="label">
+						<span>Event Status</span>
+						<select class="select" bind:value={statusFilter}>
+							<option value="upcoming">Upcoming</option>
+							<option value="canceled">Cancelled</option>
+							<option value="past">Past</option>
+						</select>
+					</label>
+
+					<button type="button" on:click={resetFilter} class="btn variant-filled-surface">
+						<span><FilterOffIcon></FilterOffIcon></span>
+						<span>Reset Filter</span>
+					</button>
+				</form>
+
+			</svelte:fragment>
+		</AccordionItem>
+	</Accordion>
+
 	<div class="grid grid-cols-1 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 gap-4">
 		{#if data.events }
 			{#each data.events as eachEvent }
 				<div class="card p-4 bg-initial">
-					<header class="card-header"><span class="font-bold text-xl">{eachEvent['event_name']}</span></header>
+					<header class="card-header">
+						<span class="font-bold text-xl">{eachEvent['event_name']}</span>
+					</header>
 					<section class="p-4 space-y-4">
 						<div class="h-4">
 							<p class="truncate ...">{eachEvent['description']}</p>
@@ -20,13 +105,13 @@
 							<div class="flex-auto">
 								<dt><CalendarClock></CalendarClock></dt>
 								<dd>
-									<span>{eachEvent['start_datetime']} - {eachEvent['end_datetime']}</span>
+									<span>{eachEvent['start_datetime']}</span>
 								</dd>
 							</div>
 							<div class="flex-auto">
-								<dt><DurationIcon></DurationIcon></dt>
+								<dt><StatusIcon></StatusIcon></dt>
 								<dd>
-									<span>{eachEvent['duration']}</span>
+									<span class="uppercase chip w-24 font-semibold {checkStatus(eachEvent['status'])}">{eachEvent['status']}</span>
 								</dd>
 							</div>
 
