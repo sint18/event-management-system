87e0abc908c8b2cb1a729b716be33f0a6f67f495
sint18 <htoohtoo.slh@gmail.com>
2024-04-15 15:56:41 +0100

    job schedule updated

--- src/lib/server/scheduler.ts
+++ src/lib/server/scheduler.ts
@@ -4,7 +4,7 @@ import * as db from '$lib/server/db';
 let currentJob: Job | null = null
 export function startJob() {
 	if (!currentJob) {
-		currentJob = scheduleJob('* * * * *', async function() {
+		currentJob = scheduleJob('0 1 * * *', async function() {
 			// Calling database function here
 			await db.query('call update_task();', [])
 			console.log(`Job running : ${new Date().toLocaleString()}`);
