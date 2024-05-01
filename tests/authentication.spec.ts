13ec9838f9f5b7bceb7d5dc93d1686a88862f6cb
sint18 <htoohtoo.slh@gmail.com>
2024-05-01 19:24:04 +0100

    tests updated

--- tests/authentication.spec.ts
+++ tests/authentication.spec.ts
@@ -1,93 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Admin Login', () => {
-
-	test.beforeEach(async ({ page }) => {
-		await page.goto('/admin/login');
-	});
-
-
-	test('should be able to login with the right credentials', async ({ page }) => {
-
-		// Fill data
-		await page.getByLabel('Username').fill('admin')
-		await page.getByLabel('Password').fill('admin')
-
-		await page.getByRole('button', {name: 'Login'}).click()
-
-		await page.waitForURL('http://localhost:5173/admin');
-		await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible()
-
-	})
-
-	test('should NOT be able to login with incorrect credentials', async ({ page }) => {
-		// 	Locate fields
-		await page.getByLabel('Username').fill('admin')
-		await page.getByLabel('Password').fill('wrongpassword')
-
-		await page.getByRole('button', {name: 'Login'}).click()
-
-		await expect(page.getByText('Incorrect password or username')).toBeVisible()
-	})
-
-	test('should NOT be able to login with empty username field', async ({ page }) => {
-		// 	Locate fields
-		await page.getByLabel('Username').fill('')
-
-		await page.getByRole('button', {name: 'Login'}).click()
-
-		await expect(page.getByText('Username cannot be empty')).toBeVisible()
-	})
-
-	test('should NOT be able to login with empty password field', async ({ page }) => {
-		// 	Locate fields
-		await page.getByLabel('Username').fill('test')
-
-		await page.getByRole('button', {name: 'Login'}).click()
-
-		await expect(page.getByText('Password cannot be empty')).toBeVisible()
-	})
-})
-
-test.describe('Admin account information and log out', () => {
-
-	test.beforeEach(async ({ page }) => {
-		await page.goto('/admin/login');
-
-		await page.getByLabel('Username').fill('admin')
-		await page.getByLabel('Password').fill('admin')
-
-		await page.getByRole('button', {name: 'Login'}).click()
-
-		// await page.goto('/admin')
-		await page.waitForURL('**/admin');
-		await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible()
-	});
-
-	test('should display admin account information', async ({ page }) => {
-
-		await page.getByRole('link', { name: 'admin'}).click()
-		await page.waitForURL('/admin/users/12')
-		await expect(page.getByRole('heading', { name: 'User Information'})).toBeVisible()
-	})
-
-	test('should display log out confirmation box', async ({ page }) => {
-
-		await page.getByRole('button', { name: 'Log Out'}).click()
-		await expect(page.getByText('Do you want to Log Out of your account?')).toBeVisible()
-	})
-
-	test('should be able to log out', async ({ page }) => {
-
-		await page.getByRole('button', { name: 'Log Out'}).click()
-		await expect(page.getByText('Do you want to Log Out of your account?')).toBeVisible()
-		await page.getByTestId('modal').getByRole('button', {name: 'Log Out'}).click()
-		await page.waitForURL('/admin/login')
-		await expect(page.getByRole('heading', { name: 'Event Management System'})).toBeVisible()
-	})
-})
-
-test.describe('User Login', () => {
+test.describe('User authentication', () => {
 
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/login');
@@ -133,6 +46,28 @@ test.describe('User Login', () => {
 
 		await expect(page.getByText('Password cannot be empty')).toBeVisible()
 	})
+
+	test('should be able to log out', async ({ page }) => {
+		// 	Locate fields
+		await page.getByLabel('Username').fill('testuser')
+		await page.getByLabel('Password').fill('testuser')
+
+		await page.getByRole('button', {name: 'Login'}).click()
+
+		await page.waitForURL('/');
+		await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible()
+		await expect(page.getByRole('button', { name: 'Log Out' })).toBeVisible();
+
+		await page.getByRole('button', { name: 'Log Out' }).click();
+		await expect(page.getByText('Do you want to Log Out of')).toBeVisible();
+		await page.getByTestId('modal').getByRole('button', { name: 'Log Out' }).click();
+
+		await page.waitForURL('/login');
+		await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+	})
+
+
 })
 
 
--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect, type Page } from '@playwright/test';
+import { goto } from '$app/navigation';
 
-test.describe('User routes', () => {
+test.describe('should be able to navigate to different routes WITHOUT logging in', () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/');
 	});
@@ -29,6 +30,58 @@ test.describe('User routes', () => {
 
 });
 
+test.describe('should be able to navigate to different routes after logging in', () => {
+	test.beforeEach(async ({ page }) => {
+		await login(page)
+	});
+
+	test('home page', async ({ page }) => {
+		await page.getByRole('link', { name: 'Home' }).click();
+		await page.waitForURL('/');
+
+		await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Log Out' })).toBeVisible();
+	});
+
+	test('bookings page', async ({ page }) => {
+		await page.getByRole('link', { name: 'Bookings' }).click();
+		await page.waitForURL('/bookings');
+		await expect(page.getByRole('heading', { name: 'Bookings' })).toBeVisible();
+
+	});
+
+	test('account page', async ({ page }) => {
+		await page.getByRole('link', { name: 'Account' }).click();
+		await page.waitForURL('/account');
+
+		await expect(page.getByRole('heading', { name: 'Account Details' })).toBeVisible();
+
+	});
+
+});
+
+test.describe('View event information', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/');
+	});
+
+	test('should display event information correctly', async ({ page }) => {
+		await page.goto('/event/9')
+		await expect(page.getByRole('heading', { name: 'Test Undefined Event' })).toBeVisible();
+		await expect(page.getByRole('heading', { name: 'Description' })).toBeVisible();
+		await expect(page.getByRole('cell', { name: 'Start Date' })).toBeVisible();
+		await expect(page.getByRole('cell', { name: 'End Date' })).toBeVisible();
+		await expect(page.getByRole('cell', { name: 'Duration' })).toBeVisible();
+		await expect(page.getByRole('cell', { name: 'Location' })).toBeVisible();
+		await expect(page.getByRole('cell', { name: 'Organizer', exact: true })).toBeVisible();
+		await expect(page.getByRole('cell', { name: 'Organizer Description' })).toBeVisible();
+		await expect(page.getByRole('cell', { name: 'Category' })).toBeVisible();
+		await expect(page.getByRole('cell', { name: 'Status' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Book Event' })).toBeVisible();
+	});
+
+})
+
 test.describe('Book an event', () => {
 
 	test.beforeEach(async ({ page }) => {
@@ -44,7 +97,7 @@ test.describe('Book an event', () => {
 		await expect(page.getByRole('heading', { name: 'Description' })).toBeVisible();
 	});
 
-	test('should not be able to book an expired event', async ({ page }) => {
+	test('should NOT be able to book an expired event', async ({ page }) => {
 
 		await page.locator('div.card')
 			.filter({ has: page.getByText('New Test Event') })
@@ -119,12 +172,20 @@ async function login(page: Page){
 
 	await page.waitForURL('/');
 	await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible()
+	await expect(page.getByText('testuser')).toBeVisible()
 }
 test.describe('Booking and account information', () => {
 
 	test.beforeEach(async ({ page }) => {
 		await login(page)
 	});
+
+	test('should display booking page  correctly', async({ page }) => {
+		await page.goto('/bookings')
+		await expect(page.getByRole('heading', { name: 'Present' })).toBeVisible();
+		await expect(page.getByRole('heading', { name: 'Past' })).toBeVisible();
+		await expect(page.getByRole('link', { name: 'Test Undefined Event 01/01/' })).toBeVisible();
+	})
 
 	test('should display booking information', async ({ page }) => {
 		await page.getByRole('link', { name: 'Bookings' }).click();
@@ -140,8 +201,15 @@ test.describe('Booking and account information', () => {
 		await page.getByRole('link', { name: 'Account' }).click();
 		await page.waitForURL('/account')
 
-		await expect(page.locator('dl')).toContainText('Name TEST USER');
-		await expect(page.locator('dl')).toContainText('Username testuser');
+		await expect(page.getByRole('heading', { name: 'Account Details' })).toBeVisible();
+		await expect(page.getByText('Name', { exact: true })).toBeVisible();
+		await expect(page.getByText('TEST USER')).toBeVisible();
+		await page.getByText('Username').click();
+		await expect(page.locator('#page-content').getByText('testuser', { exact: true })).toBeVisible();
+		await expect(page.getByText('Email', { exact: true })).toBeVisible();
+		await expect(page.getByText('testuser@gmail.com')).toBeVisible();
+		await expect(page.getByText('Points Earned')).toBeVisible();
+		await expect(page.getByText('0')).toBeVisible();
 
 		await expect(page.getByRole('heading', { name: 'Settings' })).toBeVisible();
 		await expect(page.getByRole('button', { name: 'Change Password' })).toBeVisible();
